db68f0adf4dc6d118ef9b0edbe9cb6392867f490
Ivor <ikojun00@fesb.hr>
2024-06-13 11:05:47 +0200

    feats: dynamic routing and hamburger menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const db = [
   {
@@ -94,18 +95,20 @@ export default function Home() {
       <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-20 px-10 mt-10">
         {db.map((item) => (
           <div className="flex gap-4 md:flex-col sm:items-center" key={item.id}>
-            <div>
-              <Image
-                src="/shorts.jpg"
-                alt="bla"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
-            <h3>{item.name}</h3>
-            <h2>{item.price}€</h2>
+            <Link href={`${item.id}`}>
+              <div className="bg-slate-100 p-4">
+                <Image
+                  src="/shorts.jpg"
+                  alt="bla"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <h3>{item.name}</h3>
+              <h2>{item.price}€</h2>
+            </Link>
           </div>
         ))}
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,16 @@
 
 import React, { useState } from "react";
 import HamburgerIcon from "./icons/HamburgerIcon";
+import Link from "next/link";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const toggleHamburgerMenu = () => {
     setIsOpen(!isOpen);
+    !isOpen
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
   };
 
   const hamburgerIcon = (
@@ -19,7 +24,23 @@ export default function Navbar() {
     <div className="sticky z-10 bg-white top-0 left-0 border-b-2">
       <div className="flex justify-between p-5 border-b-2">
         {hamburgerIcon}
-        <h1 className="text-3xl">Style Haven</h1>
+        <div
+          className={`${
+            !isOpen ? "hidden" : "fixed"
+          } inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`}
+        >
+          <div className="h-full max-w-screen-sm bg-white p-5 flex flex-col gap-8">
+            {hamburgerIcon}
+            <div className="flex flex-col gap-4 p-1">
+              <h2 className="text-2xl">Clothing</h2>
+              <h2 className="text-2xl">Shoes</h2>
+              <h2 className="text-2xl">Accessories</h2>
+            </div>
+          </div>
+        </div>
+        <Link href="/">
+          <h1 className="text-3xl">Style Haven</h1>
+        </Link>
         <div></div>
       </div>
     </div>
